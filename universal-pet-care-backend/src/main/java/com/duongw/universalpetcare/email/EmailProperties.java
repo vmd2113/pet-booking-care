1f15e3283c3af8821ed1194c1937ef5ede14d2bf
Duong <85083734+vmd2113@users.noreply.github.com>
2024-11-07 10:37:53 +0700

    Update EmailProperties.java

--- universal-pet-care-backend/src/main/java/com/duongw/universalpetcare/email/EmailProperties.java
+++ universal-pet-care-backend/src/main/java/com/duongw/universalpetcare/email/EmailProperties.java
@@ -6,9 +6,9 @@ import lombok.Data;
 public class EmailProperties {
     public static final String DEFAULT_HOST = "smtp.gmail.com";
     public static final int DEFAULT_PORT = 587;
-    public static final String DEFAULT_SENDER = "chungminh9999@gmail.com";
-    public static final String DEFAULT_USERNAME = "chungminh9999@gmail.com";
-    public static final String DEFAULT_PASSWORD = "gwhqnpnzndgzvhdd";
+    public static final String DEFAULT_SENDER = "[your_gmail]";
+    public static final String DEFAULT_USERNAME = "[your_gmail]";
+    public static final String DEFAULT_PASSWORD = "[your_application_password]";
     public static final boolean DEFAULT_AUTH = true;
     public static final boolean DEFAULT_STARTTLS = true;
 }
